7ad3e4247b496900d1e7005a42a9bb650ef09934
yonxiek <yonxiekbyarm@gmail.com>
2026-06-13 20:10:54 +0300

    Install script: Added auto Oh-My-Zsh and plugins

--- install.sh
+++ install.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
 
-# Путь к папке с дотфайлами (где лежит сам скрипт)
 DOTFILES_DIR=$(pwd)
+ZSH_PLUGINS_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins"
 
 echo "--- Начинаю установку конфигов ---"
 
-# Список папок в .config, которые нужно скопировать
-CONFIG_FOLDERS=("hypr" "waybar" "kitty" "fuzzel" "dunst" "nvim" "gtk-3.0" "gtk-4.0")
+# 1. Установка Oh-My-Zsh (если нет)
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Установка Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
-# 1. Работаем с .config
+# 2. Установка плагинов Zsh (автодополнения и подсветка)
+mkdir -p "$ZSH_PLUGINS_DIR"
+if [ ! -d "$ZSH_PLUGINS_DIR/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_PLUGINS_DIR/zsh-autosuggestions"
+fi
+if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting"
+fi
+
+# 3. Копирование конфигов
+CONFIG_FOLDERS=("hypr" "waybar" "kitty" "fuzzel" "dunst" "nvim" "gtk-3.0" "gtk-4.0")
 mkdir -p ~/.config
 
 for folder in "${CONFIG_FOLDERS[@]}"; do
     if [ -d "$DOTFILES_DIR/.config/$folder" ]; then
-        # Если папка уже есть в системе, делаем бэкап
-        if [ -d "$HOME/.config/$folder" ]; then
-            echo "Бэкап существующей папки $folder в $folder.bak"
-            rm -rf "$HOME/.config/$folder.bak"
-            mv "$HOME/.config/$folder" "$HOME/.config/$folder.bak"
-        fi
-        echo "Установка конфига: $folder"
+        echo "Ставлю конфиг: $folder"
+        rm -rf "$HOME/.config/$folder" # Удаляем старое, чтобы не было путаницы
         cp -r "$DOTFILES_DIR/.config/$folder" "$HOME/.config/"
     fi
 done
 
-# 2. Работаем с Zsh
+# 4. Копирование .zshrc
 if [ -f "$DOTFILES_DIR/.zshrc" ]; then
-    if [ -f "$HOME/.zshrc" ]; then
-        echo "Бэкап .zshrc в .zshrc.bak"
-        mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
-    fi
-    echo "Установка .zshrc"
+    echo "Ставлю .zshrc"
     cp "$DOTFILES_DIR/.zshrc" "$HOME/"
 fi
 
-# 3. Работаем с курсорами
+# 5. Тема курсора
 mkdir -p ~/.icons/default
 if [ -f "$DOTFILES_DIR/.icons/default/index.theme" ]; then
-    echo "Установка темы курсора"
     cp "$DOTFILES_DIR/.icons/default/index.theme" "$HOME/.icons/default/"
 fi
 
-echo "--- Установка завершена! ---"
-echo "Не забудь установить программы из packages.txt, если ты на новой системе."
+echo "--- Готово! Перезапусти терминал. ---"
